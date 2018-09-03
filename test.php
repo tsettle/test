e57457bebc3d376b7b8785d3fc12f9293c5326ef
Troy Settle <troy.settle@gmail.com>
2018-09-03 16:39:48 -0400

    Way too damned stupid... I should give up tryign to code.

--- test.php
+++ test.php
@@ -4,4 +4,6 @@ require_once __DIR__.'/bootstrap.php';
 use Scanner\Device;
 use Scanner\CIDR;
 
-print_r(get_declared_classes());
+print_r(CIDR::cidrToRange("192.168.24.0/27"));
+
+// print_r(get_declared_classes());
